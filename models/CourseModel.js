e42aa9bbcae4f9c1ec7a599d89ab524c61d84bf1
kbesseghir <k.besseghir@esi-sba.dz>
2023-01-15 14:46:12 +0100

    commit

--- models/CourseModel.js
+++ models/CourseModel.js
@@ -1,1 +1,14 @@
+const  mongoose = require('mongoose');
 
+const courseSchema = new mongoose.Schema({
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: Number,
+      required: true
+    }
+  }, { collection: 'courses' });
+
+module.exports= mongoose.model('Course',courseSchema);
